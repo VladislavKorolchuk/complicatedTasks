6f84cf055aee7fa7b857eada165fb04e4962da44
93294 <9329496@mail.ru>
2022-09-29 23:07:51 +0300

    3 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        task1();
+        task2();
+        task3();
     }
+
+    public static void task1() {
+        int a = 12, b = 27, c = 44, d = 15, e = 9;
+        int eresult = a * (b + (c - d * e));
+        System.out.println(~eresult + 1); // или System.out.println(-eresult);
+    }
+    public static void task2() {
+        int a = 5, b = 7;
+        // меняем местами пошагово с помощью XOR
+        a = a ^ b;
+        b = b ^ a;
+        a = a ^ b;
+        System.out.println("a=" + a + "  b=" + b);
+    }
+    public static void task3() {
+        int a = 605;
+        int b = (a / 10) % 10;
+        System.out.println(b);
+    }
+    public static void task4() {
+        int a = 605;
+        int b = (a / 10) % 10;
+        System.out.println(b);
+    }
+
 }
